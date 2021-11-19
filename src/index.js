78710a0b4a5749f1acc772d6faebee8bd7746a5e
一只有梦想的咸鱼 <2511907843@qq.com>
2021-11-19 14:43:02 +0800

    perfect src/index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,5 @@ export default [
     name: 'hello-world', // 名称不可重复
     component: HelloWorld,
     type: 'applicationPage'
-  },
-  {
-    name: 'hello-world2',
-    component: HelloWorld,
-    type: 'applicationPage'
   }
 ]
